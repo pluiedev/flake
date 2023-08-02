999308dd7bbc0c95a632b368193d0a17ec850ef4
Leah Amelia Chen <hi@pluie.me>
2023-08-02 21:44:35 +0800

    alejandraize

--- systems/fix-246195.nix
+++ systems/fix-246195.nix
@@ -1,11 +1,13 @@
-{ config, lib, pkgs, ...}:
-
-with lib;
-
-let
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
   cfg = config.boot.loader.systemd-boot;
   efi = config.boot.loader.efi;
-  python3 = pkgs.python3.withPackages (ps: [ ps.packaging ]);
+  python3 = pkgs.python3.withPackages (ps: [ps.packaging]);
   systemdBootBuilder = pkgs.substituteAll {
     src = ./systemd-boot-builder.py;
     isExecutable = true;
@@ -13,8 +15,14 @@ let
     systemd = config.systemd.package;
     nix = config.nix.package.out;
     timeout = optionalString (config.boot.loader.timeout != null) config.boot.loader.timeout;
-    editor = if cfg.editor then "True" else "False";
-    configurationLimit = if cfg.configurationLimit == null then 0 else cfg.configurationLimit;
+    editor =
+      if cfg.editor
+      then "True"
+      else "False";
+    configurationLimit =
+      if cfg.configurationLimit == null
+      then 0
+      else cfg.configurationLimit;
 
     inherit (cfg) consoleMode graceful;
     inherit (efi) efiSysMountPoint canTouchEfiVariables;
@@ -23,31 +31,34 @@ let
     memtest86 = optionalString cfg.memtest86.enable pkgs.memtest86-efi;
     netbootxyz = optionalString cfg.netbootxyz.enable pkgs.netbootxyz-efi;
 
-    copyExtraFiles = pkgs.writeShellScript "copy-extra-files" ''
-      empty_file=$(${pkgs.coreutils}/bin/mktemp)
+    copyExtraFiles = pkgs.writeShellScript "copy-extra-files" ''      
+            empty_file=$(${pkgs.coreutils}/bin/mktemp)
 
-      ${concatStrings (mapAttrsToList (n: v: ''
-        ${pkgs.coreutils}/bin/install -Dp "${v}" "${efi.efiSysMountPoint}/"${escapeShellArg n}
-        ${pkgs.coreutils}/bin/install -D $empty_file "${efi.efiSysMountPoint}/efi/nixos/.extra-files/"${escapeShellArg n}
-      '') cfg.extraFiles)}
+            ${concatStrings (mapAttrsToList (n: v: ''
+          ${pkgs.coreutils}/bin/install -Dp "${v}" "${efi.efiSysMountPoint}/"${escapeShellArg n}
+          ${pkgs.coreutils}/bin/install -D $empty_file "${efi.efiSysMountPoint}/efi/nixos/.extra-files/"${escapeShellArg n}
+        '')
+        cfg.extraFiles)}
 
-      ${concatStrings (mapAttrsToList (n: v: ''
-        ${pkgs.coreutils}/bin/install -Dp "${pkgs.writeText n v}" "${efi.efiSysMountPoint}/loader/entries/"${escapeShellArg n}
-        ${pkgs.coreutils}/bin/install -D $empty_file "${efi.efiSysMountPoint}/efi/nixos/.extra-files/loader/entries/"${escapeShellArg n}
-      '') cfg.extraEntries)}
+            ${concatStrings (mapAttrsToList (n: v: ''
+          ${pkgs.coreutils}/bin/install -Dp "${pkgs.writeText n v}" "${efi.efiSysMountPoint}/loader/entries/"${escapeShellArg n}
+          ${pkgs.coreutils}/bin/install -D $empty_file "${efi.efiSysMountPoint}/efi/nixos/.extra-files/loader/entries/"${escapeShellArg n}
+        '')
+        cfg.extraEntries)}
     '';
   };
 
-  checkedSystemdBootBuilder = pkgs.runCommand "systemd-boot" {
-    nativeBuildInputs = [ pkgs.mypy python3 ];
-  } ''
-    install -m755 ${systemdBootBuilder} $out
-    mypy \
-      --no-implicit-optional \
-      --disallow-untyped-calls \
-      --disallow-untyped-defs \
-      $out
-  '';
+  checkedSystemdBootBuilder =
+    pkgs.runCommand "systemd-boot" {
+      nativeBuildInputs = [pkgs.mypy python3];
+    } ''
+      install -m755 ${systemdBootBuilder} $out
+      mypy \
+        --no-implicit-optional \
+        --disallow-untyped-calls \
+        --disallow-untyped-defs \
+        $out
+    '';
 
   finalSystemdBootBuilder = pkgs.writeScript "install-systemd-boot.sh" ''
     #!${pkgs.runtimeShell}
@@ -56,4 +67,4 @@ let
   '';
 in {
   system.build.installBootLoader = lib.mkForce finalSystemdBootBuilder;
-} 
+}
