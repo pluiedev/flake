46d78583b43d16863e166101120497d85dd7ad8c
Leah Amelia Chen <hi@pluie.me>
2023-09-15 21:36:18 +0200

    fettuccine: relocate to berlin

--- systems/fettuccine/default.nix
+++ systems/fettuccine/default.nix
@@ -16,7 +16,6 @@
       plasma.enable = true;
     };
     patch.fix-246195 = true;
-    enableChineseMirrors = true;
   };
 
   hardware.bluetooth.enable = true;
@@ -25,8 +24,14 @@
     powerManagement.enable = true;
 
     prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
   };
+
+  time.timeZone = "Europe/Berlin";
 }
