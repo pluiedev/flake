d1fc50c7614f31dcfa0948df31091bd244a08211
Leah Amelia Chen <hi@pluie.me>
2023-11-07 09:22:04 +0100

    pkgs: remove home-grown vesktop

--- nixpkgs/overlay.nix
+++ nixpkgs/overlay.nix
@@ -1,6 +1,3 @@
 final: prev: {
   catppuccin-qtct = final.callPackage ./pkgs/catppuccin-qtct.nix {};
-
-  hyprshot = final.callPackage ./pkgs/hyprshot.nix {};
-  vesktop = final.callPackage ./pkgs/vesktop-0.4.nix {};
 }
