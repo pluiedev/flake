1cb9c2c40dab282ef25c9c24e9f3b2c9dd489f11
Leah Amelia Chen <hi@pluie.me>
2024-04-27 17:30:11 +0200

    roles(catppuccin): use global enable

--- roles/catppuccin/default.nix
+++ roles/catppuccin/default.nix
@@ -27,28 +27,16 @@ in {
   };
 
   config = mkIf enable {
-    catppuccin = {inherit flavour;};
+    catppuccin = {
+      inherit flavour;
+      enable = true;
+    };
     hm = {
-      catppuccin = {inherit accent flavour;};
-
       imports = [catppuccin.homeManagerModules.catppuccin];
 
-      i18n.inputMethod.fcitx5.catppuccin.enable = true;
-
-      programs = {
-        bat.catppuccin.enable = mkDefault true;
-        fish.catppuccin.enable = mkDefault true;
-        kitty.catppuccin.enable = mkDefault true;
-
-        fuzzel.settings.colors = {
-          background = "11111bff";
-          text = "cdd6f4ff";
-          match = "a6e3a1ff";
-          selection = "f5e0dcff";
-          selection-text = "1e1e2eff";
-          selection-match = "1f7f18ff";
-          border = "eba0acee";
-        };
+      catppuccin = {
+        inherit accent flavour;
+        enable = true;
       };
     };
   };
