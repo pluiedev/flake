205005fa7b61cb497c14f1b618ad4fa300f9e919
Leah Amelia Chen <hi@pluie.me>
2025-07-03 09:31:52 +0000

    fettuccine: add actual swap space

--- systems/fettuccine/hardware-configuration.nix
+++ systems/fettuccine/hardware-configuration.nix
@@ -91,7 +91,9 @@
     ];
   };
 
-  swapDevices = [ ];
+  swapDevices = [
+    { device = "/swap/swapfile"; }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
