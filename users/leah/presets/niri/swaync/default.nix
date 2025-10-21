1317da5e4ca69c93b10c3bb2cc03ce30d787ffdb
Leah Amelia Chen <hi@pluie.me>
2025-10-21 23:29:42 +0200

    niri: graphical-session-pre is unnecessary

--- users/leah/presets/niri/swaync/default.nix
+++ users/leah/presets/niri/swaync/default.nix
@@ -5,7 +5,7 @@
 {
   hjem.users.leah = {
     packages = [ pkgs.swaynotificationcenter ];
-    systemd.targets.graphical-session-pre.wants = [ "swaync.service" ];
+    systemd.targets.graphical-session.wants = [ "swaync.service" ];
 
     xdg.config.files = {
       "swaync/style.css".source = ./style.css;
--- users/leah/presets/niri/waybar/default.nix
+++ users/leah/presets/niri/waybar/default.nix
@@ -5,15 +5,16 @@
 let
   jsonFormat = pkgs.formats.json { };
   config = import ./config.nix args;
+  configFile = jsonFormat.generate "waybar-config.jsonc" config;
 in
 {
   hjem.users.leah = {
     packages = [ pkgs.waybar ];
-    systemd.targets.graphical-session-pre.wants = [ "waybar.service" ];
+    systemd.targets.graphical-session.wants = [ "waybar.service" ];
 
     xdg.config.files = {
       "waybar/style.css".source = ./style.css;
-      "waybar/config.jsonc".source = jsonFormat.generate "waybar-config.jsonc" config;
+      "waybar/config.jsonc".source = configFile;
     };
   };
 }
