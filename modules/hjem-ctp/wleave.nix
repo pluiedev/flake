4e1a39e7b51af1573472a9b850c485b692c1cb84
Leah Amelia Chen <hi@pluie.me>
2025-10-21 16:37:46 +0200

    hjem-ctp: remove redundant wleave icons

--- modules/hjem-ctp/wleave.nix
+++ modules/hjem-ctp/wleave.nix
@@ -15,22 +15,6 @@ let
     rev = "b690cee13b944890e43a5fb629ccdff86cffbbb3";
     hash = "sha256-QUSDx5M+BG7YqI4MBsOKFPxvZHQtCa8ibT0Ln4FPQ7I=";
   };
-
-  # Generate the CSS required to override all icons
-  # with their Catppuccin-ified counterparts
-  iconsCss =
-    lib.concatMapStrings
-      (btn: ''
-        #${btn} { background-image: url("${src}/icons/wleave/${cfg.flavor}/${cfg.accent}/${btn}.svg"); }
-      '')
-      [
-        "lock"
-        "logout"
-        "suspend"
-        "hibernate"
-        "shutdown"
-        "reboot"
-      ];
 in
 {
   options.ctp.wleave = ctp-lib.mkCatppuccinOptions "wleave" {
@@ -38,9 +22,8 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    xdg.config.files."wleave/style.css".source = pkgs.concatText "wleave-style.css" [
-      "${src}/themes/${cfg.flavor}/${cfg.accent}.css"
-      (pkgs.writeText "wleave-icons.css" iconsCss)
-    ];
+    xdg.config.files = {
+      "wleave/style.css".source = "${src}/themes/${cfg.flavor}/${cfg.accent}.css";
+    };
   };
 }
