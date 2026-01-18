84d64a5d346caeae689c64576c1b2c105c2ef786
Leah Amelia Chen <hi@pluie.me>
2026-01-18 13:11:29 +0800

    bye GitHub! (#1)

--- users/leah/presets/plasma/default.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }:
-{
-  imports = [
-    ./sddm.nix
-    ./settings
-  ];
-}
--- users/leah/presets/plasma/sddm.nix
+++
@@ -1,8 +0,0 @@
-{
-  services.displayManager.sddm = {
-    enable = true;
-    # TODO: use ibus if ibus is enabled, or fallback to compose, NEVER use virtual keyboard
-    settings.General.InputMethod = "compose";
-  };
-  catppuccin.sddm.enable = false; # awaiting port
-}
